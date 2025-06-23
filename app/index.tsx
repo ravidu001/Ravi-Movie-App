8421fa273ad2672fc75465b7950561ae41786b9f
ravidu001 <ravidupeiris2002@gmail.com>
2025-06-23 14:35:02 +0530

    fix: update welcome message to include app name

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,7 @@ import { Link } from "expo-router";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-primary font-bold">Welcome!</Text>
+      <Text className="text-5xl text-primary font-bold">Welcome to my mobile movie app!</Text>
     </View>
   );
 }
